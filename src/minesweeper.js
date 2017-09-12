ec53988adc4625e600e047b5c6a12929a78fac63
Martin Leroux <tintin_151@hotmail.com>
2017-09-11 21:23:03 -0400

    Create a Game Board

---
+++ src/minesweeper.js
@@ -0,0 +1,15 @@
+const printBoard = (board) => {
+	console.log('Current Baord:');
+	console.log(board[0].join(' | '));
+	console.log(board[1].join(' | '));
+	console.log(board[2].join(' | '));
+};
+const board=[
+		['','',''],
+		['','',''],
+		['','','']
+	];
+printBoard(board);
+board[0][1]='1';
+board[2][2]='B';
+printBoard(board);
